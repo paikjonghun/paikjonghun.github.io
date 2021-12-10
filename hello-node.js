6f735a6241bba64ecae3786b1fadd0e90b55bd36
paikjonghun <hun3072@gmail.com>
2021-12-11 00:04:57 +0900

    [7th commit] 블로그 만들기

--- hello-node.js
+++ hello-node.js
@@ -36,9 +36,44 @@ const md = require("markdown-it")({
   }
 });
 
-contentFiles.map(file => {
-    const body = fs.readFileSync(`./contents/${file}`, "utf8");
-    const convertedBody = md.render(body);
-    console.log(convertedBody);
-  });
 
+  const dir = "./deploy";
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+  
+  const ejs = require("ejs");
+
+  const layoutHtmlFormat = fs.readFileSync(
+    "./templates/layout_format.html",
+    "utf8"
+  );
+  const articleHtmlFormat = fs.readFileSync(
+    "./templates/article_format.html",
+    "utf8"
+  );
+
+
+
+  
+// 확장자를 제외한 파일 이름을 얻는 함수
+getHtmlFileName = file => {
+    return file.slice(0, file.indexOf(".")).toLowerCase();
+  };
+  // deploy 폴더 안에 넣은 파일의 리스트
+  const deployFiles = [];
+  // map함수로 content안에 있는 파일들을 반복문을 돌면서 deploy안에 html파일 생성
+  contentFiles.map(file => {
+    const body = fs.readFileSync(`./contents/${file}`, "utf8");
+  
+    const convertedBody = md.render(body);
+    const articleContent = ejs.render(articleHtmlFormat, {
+      body: convertedBody
+    });
+    const articleHtml = ejs.render(layoutHtmlFormat, {
+      content: articleContent
+    });
+    const fileName = getHtmlFileName(file);
+    fs.writeFileSync(`./deploy/${fileName}.html`, articleHtml);
+    deployFiles.push(fileName);
+  });
